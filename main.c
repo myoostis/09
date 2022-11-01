3c2907494e36c9752d15d8be32ae2ee3cacabd81
myoostis <parkgin2011@naver.com>
2022-11-01 16:46:44 +0900

    03_1

--- main.c
+++ main.c
@@ -2,21 +2,23 @@
 #include <stdlib.h>
 #define SIZE 5
 
-int main(void) {
-	
+int main(void){
 	int i;
 	int grade[SIZE];
-	printf("5명의 점수를 입력하세요. \n");
-		
+	int score[SIZE];
+	
 	for(i=0; i<SIZE; i++){
-		scanf("%d", &grade[i]);
+		grade[i] = rand() % 100;
 	}
 	
-	for(i=0; i<5; i++){
-	 printf("grade[%d] = %d\n", i, grade[i]);	
+	for(i=0; i<SIZE; i++){
+		score[i] = grade[i];
+    }
+    
+	for(i=0; i<SIZE; i++){
+		printf("score[%d] = %d (%d)\n", i, score[i], grade[i]);
 	}
-
+	
 	return 0;
 }
-
 
